37abf543c5192634351a6fa8951aeb645372b2c0
Kyle Bridburg <kbridbur@mit.edu>
2016-08-24 16:51:55 -0400

    Outline Classes

--- src/spiked-tea.js
+++ src/spiked-tea.js
@@ -19,52 +19,65 @@ class Neuron{
     this.threshhold = threshhold;
   }
   
-  OnRecieveInput(input){
-    time_recieved = //something to tell the time
+  GetOutput(inputs){
     //find the value of time that the inputs, when passed into the input function
-    this.inputs.push(input);
-    for (i = 0; i < /*some amount of time so that the newest input = 0*/;i += /*Increment by milliseconds*/){
+    for (i = 0; i < 64;i++){
       summation = 0;
       for (j = 0; j < this.inputs.length; j++){
-        summation += GetTimeAlteredInputValue(this.inputs[j][0], i - this.inputs[j][0]);
+        weight = this.inputs[j][1];
+        time_recieved = this.inputs[j][0];
+        summation += time_recieved === null ? 0 : GetTimeAlteredInputValue(weight, i - time_recieved);
       }
       if (summation >= threshhold){
-        Output(i);
-        return;
+        return i;
       }
     }
+  return null;
   }
-  
-  Output(time : float){
-    
-    
-    this.inputs = [];
-  }
-
 }
 
 class Cluster{
   //group of neurons all on the same global time scale
+  constructor(neurons, connections, inputs){
+    this.neurons = neurons;
+    this.connections = connections;
+    this.inputs = NormalizeInputs(inputs);
+  }
   
+  DistributeInputs(){
+    //Distribute inputs to the proper neurons based on connections
+  }
+  
+  NormalizeInputs(inputs){
+    //Take the inputs which are in the form of times and normalize them (remember some will be null)
+    min = minimum(inputs)
+  }
+  
+  GetOutputs(){
+    //Given the inputs get the output of each
+    outputs = [];
+    for (i = 0; i< this.neurons.length; i++){
+      outputs.push(this.neurons[i].GetOutput());
+    }
+    return outputs;
+  }
 }
 
 class Inhibitor{
   //culls inputs between layers of neurons based on strength
-  
+  constructor(){
+    
+  }
 }
 
 class Layer{
   //contains clusters, may have multiple time scales within but has no interconnecting neurons
-  
+  constructor(inhibitor : Inhibitor, clusters, inputs){
+    
+  }
 }
 
-
-
-
 function EvaluateInput(input : float, time_since_recieved : float) : float{
-  if (input < 0){
-    return 0;
-  }
   if (time_since_recieved < 0){
     return 0;
   } else if (time_since_recieved < 10+2(input-1)){
@@ -73,6 +86,15 @@ function EvaluateInput(input : float, time_since_recieved : float) : float{
     return input*(1-time_since_recieved/50);
   } else {
     return 0;
-  }
-  
+  } 
+}
+
+function minimum(a) {
+  var min=a[0]; for(var i=0,j=a.length;i<j;i++){min = (a[i] < min) && (a[i] !== null) ? a[i] : min;}
+  return min;
+}
+
+function maximum(a) {
+  var max=a[0]; for(var i=0,j=a.length;i<j;i++){max = (a[i] > max) && (a[i] !== null) ? a[i] : max;}
+  return max;
 }
